efd6f082b8cae12033ec7bf7f5a2b371bb132f71
It's MD <90133844+Mohammadhussain-Ambaliyasana@users.noreply.github.com>
2024-04-30 19:31:16 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,32 @@
+x = 10
+print (type(x))
+
+x = 10.5
+print (type(x))
+
+x = 10j
+print (type(x))
+
+x = True
+print (type(x))
+
+x = {10 , 20}
+print (type(x))
+
+x = frozenset({10 , 20})
+print (type(x))
+
+x = (10,20,30)
+print (type(x))
+
+x = [10,20,30]
+print (type(x))
+
+x = {1:20,2:40}
+print (type(x))
+
+x = "10"
+print (type(x))
+
+x = b"10"
+print (type(x))
